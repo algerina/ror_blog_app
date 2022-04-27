361b4a3a17234f3cc7d0ee926a17c19ef3a28e84
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-04-27 12:09:54 +0100

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
