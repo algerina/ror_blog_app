a4530fb08b1d3b66997c93400bfd07fb1e02ae65
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-02 23:31:51 +0100

    Create methods for user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
