ccfb405fe2553caf162a37b780fdcb62d74bbb91
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-03 22:43:38 +0100

    Create users view

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'sprockets-rails'
 # gem 'sqlite3', '~> 1.4'
 gem 'pg'
 
+gem 'bootstrap', '~> 5.1.3'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
 
@@ -61,6 +63,11 @@ group :development, :test do
   # Ffi
   gem 'ffi'
 end
+
+group :assets do
+  gem 'sass-rails', '~> 6.0'
+end
+
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1650784422954-9e522e77f473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
