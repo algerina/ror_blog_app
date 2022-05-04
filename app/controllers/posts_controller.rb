54cd658b627f3008ad006e57191edf8695931213
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-04 21:10:11 +0100

    Add create action to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,24 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @current_user = User.find(params[:user_id])
+    post = Post.new(post_params.merge(author_id: @current_user.id, comments_counter: 0, likes_counter: 0))
+    if post.valid?
+      post.save
+     flash[:notice] = "Post was successfully created"
+     redirect_to user_path(@current_user.id)
+    else
+      redirect_to new_user_post_path(@current_user.id)
+    end
+
+
+   end
+
+   
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
