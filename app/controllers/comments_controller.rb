70c46f82548c9d83ac001c41869bf816d70e0fca
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-04 22:05:54 +0100

    Add create action to Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
   def new
+    @comments = Comment.new
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create(post: @post, author: @user, text: params[:comment][:text] );
+
+    if @comment.valid?
+      @comment.save
+      redirect_to user_post_path(id: @post.id,user_id: @user.id)
+    else
+      redirect_to new_user_post_comment_path(@user.id, @post.id)
+    end
   end
+
+
+  def post_params
+    params.require(:comment).permit(:text)
+  end
+
+
 end
