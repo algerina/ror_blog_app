bcd39c86d74adf896205700a94bd5c6d2c73e7e6
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-06 22:09:09 +0100

    test post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Post.new(
+      author_id: 1,
+      title: 'One of my posts',
+      text: 'Between light and shadow',
+      comments_counter: 1,
+      likes_counter: 1
+    )
+  end
+  before { subject.save }
+
+  it 'should have a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'title maximum length is 250' do
+    subject.title = 'a' * 251
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes counter must be an integer greater or equal to 0' do
+    subject.likes_counter = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'comments counter must be an integer greater or equal to 0' do
+    subject.comments_counter = nil
+    expect(subject).to_not be_valid
+  end
 end
