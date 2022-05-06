6ce63c0001b8c407b96815b945e4ec8fe08417c3
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-06 23:48:36 +0100

    Update post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,6 +16,6 @@ class Post < ApplicationRecord
   end
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
