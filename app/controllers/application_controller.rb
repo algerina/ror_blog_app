a62bc886ce4e79c89f47049eec3295f79e2ec3cf
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-11 00:18:58 +0100

    Delete commented lines of code and update users show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  # def current_user
-  #   User.first
-  # end
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -16,14 +13,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # before_action :authenticate_user!
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  # end
-
-  # def update_allowed_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up) do |u|
-  #   u.permit(:name, :bio, :email, :password, :password_confirmation)
-  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @posts = @user.most_recent_posts
   end
 end
