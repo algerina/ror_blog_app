9e3a0e7d424b686297b2bad735c85a81bfe51bf0
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-11 01:30:11 +0100

    Add delete button and method for post

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,32 +5,14 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-      elsif user.present?
-        can [:read, :update, :destroy], Post, user: user 
-      end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    user ||= User.new
+    if user.is? :admin
+      can :manage, :all
+    elsif can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
+      can :read, :all
+    else
+      can :read, :all
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,17 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
+
+
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 
-  # validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
   # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
