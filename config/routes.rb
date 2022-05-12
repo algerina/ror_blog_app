3612f15a283986f36304f944011e4ea38962dba3
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-12 18:26:40 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/api/users/:user_id/posts', to: 'api#index_posts'
+  get '/api/users/:user_id/posts/:post_id/comments', to: 'api#post_comments'
+  post '/api/users/:user_id/posts/:post_id/comment', to: 'api#create_comments
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
