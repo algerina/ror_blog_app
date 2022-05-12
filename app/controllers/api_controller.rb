3291e1659f442a0fccd07308d3f1ff31cf85c8e6
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-12 20:00:02 +0100

    Fix linters issues

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,29 +1,29 @@
 class ApiController < ApplicationController
-        protect_from_forgery with: :null_session
-      
-        def index_posts
-          render json: Post.all
-        end
-      
-        def post_comments
-          render json: Comment.where(post_id: params[:post_id])
-        end
-      
-        def create_comments
-          @comment = Comment.new(comment_params)
-          @comment.post_id = params[:post_id]
-          @comment.author_id = params[:user_id]
+  protect_from_forgery with: :null_session
 
-          if @comment.save
-            render json: { status: 201, message: 'Comment creation success!', content: { comment: @comment } }
-          else
-            render json: @comment.errors, status: :bad_request, message: 'Failed!'
-          end
+  def index_posts
+    render json: Post.all
+  end
 
-        end
+  def post_comments
+    render json: Comment.where(post_id: params[:post_id])
+  end
+
+  def create_comments
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+    @comment.author_id = params[:user_id]
+
+    if @comment.save
+      render json: { status: 201, message: 'Comment creation success!', content: { comment: @comment } }
+    else
+      render json: @comment.errors, status: :bad_request, message: 'Failed!'
     end
+  end
+end
+
         private
-      
-        def comment_params
-          params.permit(:text)
-        end
+
+def comment_params
+  params.permit(:text)
+end
